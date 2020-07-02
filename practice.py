f967d6a4b07ef9e90431b88480ce1a8760191355
RAJANKUMARManglani <40788175+RAJANKUMARManglani@users.noreply.github.com>
2020-07-02 10:19:51 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,24 +1,20 @@
+import itertools
 import os
 import subprocess
+import time
 import traceback
+
+import connection as connection
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from sshtunnel import SSHTunnelForwarder
 
 subprocess.call('start', shell=True)
-print("shell launched")
 os.chdir('/Users/rajan.kumar.manglani/Documents/DB')
-print("shell launched")
+print('Navigated to Directory where ssh keys are present')
 output = subprocess.getoutput('ls')
 print(output)
 try:
-
-    # with SSHTunnelForwarder(
-    #        ('34.195.7.84', 22),
-    #       ssh_private_key="/Users/rajan.kumar.manglani/Documents/DB/test.pem",
-    #      ssh_username="rajan.k",
-    #      remote_bind_address=('fr-test.cdgrcme2rcfh.us-east-1.rds.amazonaws.com', 5432)) as server:
-    #  print("server connected")
-
     # Create an SSH tunnel
     tunnel = SSHTunnelForwarder(
         ('34.195.7.84', 22),
@@ -27,25 +23,46 @@ try:
         remote_bind_address=('fr-test.cdgrcme2rcfh.us-east-1.rds.amazonaws.com', 5432),
         local_bind_address=('localhost', 6543),  # could be any available port
     )
+    print("ssh connection made sucessfully")
     # Start the tunnel
     tunnel.start()
-    db = psycopg2.connect(
+
+
+    def show_query(title, qry):
+        print('%s' % (title))
+        cursor.execute(qry)
+        for row1 in cursor.fetchall():
+            print(row1)
+        print('')
+
+
+    connection = psycopg2.connect(
         database='farmrise', host=tunnel.local_bind_host, port=tunnel.local_bind_port,
         user='farmersfirstappuser', password='iYCgasd',
         connect_timeout=20
     )
-    curs = db.cursor()
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = connection.cursor()
     print("database connected")
-    curs.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
-    print(curs.fetchall())
-    postgreSQL_select_Query = "select * from mobile"
-
-
-
-    # Stop the tunnel
+    cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
+    print(cursor.fetchall())
+    show_query('current database', 'SELECT current_database()')
+    show_query('available databases', 'SELECT * FROM pg_database')
+    cursor.close()
+    print("cursor is closed")
+    connection.close()
+    print("PostgreSQL connection is closed")
+    # stop the tunnel
     tunnel.stop()
+    print("tunnel stopped for ssh successfully")
 
 except (Exception, psycopg2.Error) as error:
     print(traceback.format_exc())
     print("Connection Failed")
     print("Error while connecting to PostgreSQL", error)
+finally:
+
+    # closing database connection.
+    if connection:
+        connection.close()
+        print("PostgreSQL database connection is closed successfully")
